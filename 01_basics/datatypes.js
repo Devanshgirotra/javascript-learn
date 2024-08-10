4a16cfbbfd2a52f0950c81962a8b05d862fbeb62
Devanshgirotra <141931087+Devanshgirotra@users.noreply.github.com>
2024-08-10 20:33:39 +0000

    commit config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+let state;//undefined
+let states=null;//null
+
+//number =>2 to the power 53
+//bigint
+//string =>""
+//boolean=>true/false
+//null=>standalone value
+// undefined
+//symbol=>unique
+
+//object
+
+console.log(typeof(state));
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,5 @@
 console.log("devansh")
+/*
+prefer not to use var
+because of issue in block scope and functional block
+*/
