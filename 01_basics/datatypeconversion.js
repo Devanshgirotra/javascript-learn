fa20f0cfe7fed85c2f638426a4b3c5f36a104804
Devanshgirotra <141931087+Devanshgirotra@users.noreply.github.com>
2024-09-04 12:14:38 +0000

    ok

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,12 @@
+let score ="33";
+
+console.log(typeof(score));
+
+let value=Number(score);
+console.log(typeof(value));
+
+// conversion in case of boolean ??syntax:- Boolean()
+/*0=>false; 1=>true
+""=>false
+"xyz"=>true
+*/
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -3,3 +3,9 @@ console.log("devansh")
 prefer not to use var
 because of issue in block scope and functional block
 */
+
+/*  "USE STRICT" // treat all  JS code as newer version of javascript 
+*/
+
+
+//alert(3+3)//it will not run becasue we are using node js not browser
